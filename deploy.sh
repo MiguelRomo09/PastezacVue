70422e22456e108ecaacc620b67f71631c4a71ef
MiguelRomo09 <miguelromo0994@gmail.com>
2022-03-26 21:01:09 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:miguelromo09/PastezacVue.git master:gh-pages
+git push -f https://github.com/MiguelRomo09/PastezacVue.git master:gh-pages
 
 cd -
 
